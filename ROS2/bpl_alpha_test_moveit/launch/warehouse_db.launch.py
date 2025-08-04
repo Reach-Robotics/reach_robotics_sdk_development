37a0491ed12d0f56bc7b61d2557f0b05f449b069
Michele Calabria <michelecalabria80@gmail.com>
2025-08-04 14:50:55 +1000

    Remove bpl_alpha_test and bpl_alpha_test_moveit packages

--- ROS2/bpl_alpha_test_moveit/launch/warehouse_db.launch.py
+++
@@ -1,7 +0,0 @@
-from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launches import generate_warehouse_db_launch
-
-
-def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("alpha_5", package_name="bpl_alpha_test_moveit").to_moveit_configs()
-    return generate_warehouse_db_launch(moveit_config)
